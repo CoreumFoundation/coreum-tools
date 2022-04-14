a648d528a28f2f56b5c5b986e79f9025b06a7e73
Wojtek <wojtek@coreum.com>
2022-04-14 11:45:12 +0200

    `parallel` package

--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module coreum-build-tools
 
 go 1.17
 
-require go.uber.org/zap v1.21.0
+require (
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.21.0
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ pkg/parallel/recover.go
@@ -0,0 +1,42 @@
+package parallel
+
+import (
+	"context"
+	"coreum-build-tools/pkg/logger"
+	"fmt"
+	"runtime/debug"
+
+	"go.uber.org/zap"
+)
+
+// ErrPanic is the error type that occurs when a subtask panics
+type ErrPanic struct {
+	Value interface{}
+	Stack []byte
+}
+
+func (err ErrPanic) Error() string {
+	return fmt.Sprintf("panic: %s", err.Value)
+}
+
+// Unwrap returns the error passed to panic, or nil if panic was called with
+// something other than an error
+func (err ErrPanic) Unwrap() error {
+	if e, ok := err.Value.(error); ok {
+		return e
+	}
+	return nil
+}
+
+// runTask executes the task in the current goroutine, recovering from panics.
+// A panic is returned as ErrPanic.
+func runTask(ctx context.Context, task Task) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			panicErr := ErrPanic{Value: p, Stack: debug.Stack()}
+			err = panicErr
+			logger.Get(ctx).Error("Panic", zap.String("value", fmt.Sprint(p)), zap.ByteString("stack", panicErr.Stack))
+		}
+	}()
+	return task(ctx)
+}
